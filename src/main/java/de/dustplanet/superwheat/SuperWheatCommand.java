3bc4a9023a98069e36fc465c2797ae97b90371ab
xGhOsTkiLLeRx <ratchet.player@gmx.de>
2013-03-01 17:15:13 +0100

    Refractoring: Use Sun coding conventions Changed: Overall cleanup

--- src/main/java/de/dustplanet/superwheat/SuperWheatCommand.java
+++ src/main/java/de/dustplanet/superwheat/SuperWheatCommand.java
@@ -6,22 +6,25 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 public class SuperWheatCommand implements CommandExecutor {
-	private SuperWheat plugin;
+    private SuperWheat plugin;
 
-	public SuperWheatCommand(SuperWheat instance) {
-		plugin = instance;
-	}
+    public SuperWheatCommand(SuperWheat instance) {
+	plugin = instance;
+    }
 
-	public boolean onCommand (CommandSender sender, Command command, String commandLabel, String[] args) {
-		if (args.length == 0) sender.sendMessage(ChatColor.YELLOW + "Usage: /superwheat reload (or /sw reload)");
-		else if (args[0].equalsIgnoreCase("reload")) {
-			if (sender.hasPermission("SuperWheat.reload")) {
-				plugin.loadConfigAgain();
-				sender.sendMessage(ChatColor.GREEN + "SuperWheat successfully reloaded!");
-			}
-			else sender.sendMessage(ChatColor.RED + "You don't have the permission to reload SuperWheat!");
-		}
-		else sender.sendMessage(ChatColor.RED + "This argument is unkown!");
-		return true;
+    public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
+	if (args.length == 0) {
+	    sender.sendMessage(ChatColor.YELLOW + "Usage: /superwheat reload (or /sw reload)");
+	} else if (args[0].equalsIgnoreCase("reload")) {
+	    if (sender.hasPermission("SuperWheat.reload")) {
+		plugin.loadConfigAgain();
+		sender.sendMessage(ChatColor.GREEN + "SuperWheat successfully reloaded!");
+	    } else {
+		sender.sendMessage(ChatColor.RED + "You don't have the permission to reload SuperWheat!");
+	    }
+	} else {
+	    sender.sendMessage(ChatColor.RED + "This argument is unkown!");
 	}
+	return true;
+    }
 }
